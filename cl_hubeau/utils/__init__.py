f9b141d8938c91fbf53737e110497bd8e78b26ec
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2024-08-08 18:14:16 +0200

    Update __init__.py

--- cl_hubeau/utils/__init__.py
+++ cl_hubeau/utils/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from .clean_cache import clean_all_cache
-from .cities_and_departments import get_cities, get_departements
+from .cities_deps_regions import get_cities, get_departements, get_regions
 
 __all__ = ["clean_all_cache"]
