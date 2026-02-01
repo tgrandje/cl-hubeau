a1727c0b825527fdac12a012b049f948607f9bf5
Thomas Grandjean <tgrandje@gmail.com>
2026-02-01 20:04:44 +0100

    set existing campagne code
    some data may have been removed from the existing dataset

--- tests/test_watercourses_flow.py
+++ tests/test_watercourses_flow.py
@@ -105,7 +105,7 @@ def test_get_one_station_live():
 
 def test_get_one_campaign_live():
     with WatercoursesFlowSession() as session:
-        data = session.get_campaigns(code_campagne=[12])
+        data = session.get_campaigns(code_campagne=[105900])
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 1
 
